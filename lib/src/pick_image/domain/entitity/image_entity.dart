45e8fb98b19c6f3f2fabad234d5d0d1b09167051
bracinho2 <bracinho2@hotmail.com>
2022-05-19 13:21:08 -0300

    Atualização da entidade image.

--- lib/src/pick_image/domain/entitity/image_entity.dart
+++ lib/src/pick_image/domain/entitity/image_entity.dart
@@ -1,6 +1,13 @@
+import 'dart:typed_data';
+
 class PickedImage {
   final String imageName;
   final String imagePath;
+  final Uint8List bytes;
 
-  PickedImage(this.imageName, this.imagePath);
+  PickedImage(
+    this.imageName,
+    this.imagePath,
+    this.bytes,
+  );
 }
